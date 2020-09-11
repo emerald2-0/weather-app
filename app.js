e755cdc9c6b666cb2f0be837e0f7a5920ad38342
BotanicalCoder <57745557+BotanicalCoder@users.noreply.github.com>
2020-09-11 15:39:53 +0100

    fixed the bugs

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Service Worker 
+//Service Worker
 //check support:
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
@@ -9,61 +9,59 @@ if ('serviceWorker' in navigator) {
   })
 }
 
-
-
-
 //Open Weather Call
-var button = document.querySelector('.button');
-var uservalue = document.querySelector('.uservalue');
-var name = document.querySelector('.name');
-var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
-var country = document.querySelector('.country');
 
-function setData(data){ 
-var DataString = Json.stringify(data);
-localStorage.setItem("weatherForecast", "DataString") ;
-getData();
-showData(); }
+function getData() {
+  var str = localStorage.getItem("weatherForecast");
 
-function getData(){
-var str= localStorage.getItem("DataString");
-var uiData; 
-return (uiData = Json.parse(str)); }
-
-function showData(){
-getData();
-
-var [    tempResponse, nameResponse,descResponse, CntrResponse ] = uiData;
-
-return(
-name.innerHTML = nameResponse;
-  	temp.innerHTML = tempResponse;
-  	desc.innerHTML = descResponse;
-  	country.innerHTML = CntrResponse;);
-
+  if (str != null) {
+    return (arr = JSON.parse(str));
+  }
 }
 
+function showData() {
+  getData();
+  console.log(arr);
+  var [tempResponse, nameResponse, descResponse, CntrResponse] = arr;
 
-button.addEventListener('click', function(){
-	fetch('https://api.openweathermap.org/data/2.5/weather?q='+uservalue.value+'&appid=1c7a6a6e2c64b71e4e305eca93afafac')
-  .then(response => response.json())
-  .then(data => {
-  	var nameResponse = data['name'];
-  	var tempResponse = data['main']['temp'];
-  	var descResponse = data['weather'][0]['description'];
-  	var CntrResponse = data['sys']['country']
+  var name = document.querySelector(".name");
+  var desc = document.querySelector(".desc");
+  var temp = document.querySelector(".temp");
+  var country = document.querySelector(".country");
 
-var arr = [ ];
-arr.push(nameResponse, tempResponse,  descResponse, CntrResponse) ;
+  name.innerHTML = nameResponse;
+  temp.innerHTML = tempResponse;
+  desc.innerHTML = descResponse;
+  country.innerHTML = CntrResponse;
+}
 
-setData(arr);
+function setData(arr) {
+  localStorage.setItem("weatherForecast", JSON.stringify(arr));
+  getData();
+  showData();
+}
 
-  	
-  })
+var button = document.querySelector(".button");
 
- .catch(err => alert("Wrong City name!"))
-})
+button.addEventListener("click", function () {
+  var uservalue = document.querySelector(".uservalue");
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      uservalue.value +
+      "&appid=1c7a6a6e2c64b71e4e305eca93afafac"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      var nameResponse = data["name"];
+      var tempResponse = data["main"]["temp"];
+      var descResponse = data["weather"][0]["description"];
+      var CntrResponse = data["sys"]["country"];
 
+      var arr = [];
+      arr.push(nameResponse, tempResponse, descResponse, CntrResponse);
 
- 
+      setData(arr);
+    })
+
+    .catch((err) => alert("Wrong City name!"));
+});
